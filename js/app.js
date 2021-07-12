7a218da70452f1efd06503e472fe89936029fe3c
Hekk3 <timurh434@gmail.com>
2021-07-12 23:00:13 +0300

    header adaptive

--- js/app.js
+++ js/app.js
@@ -15,13 +15,38 @@ window.onload = function() {
     const burgerButton = document.querySelector('.header__burger');
     const headerRight = document.querySelector('.header__right');
     const headerClose = document.querySelector('.header__close');
+    const bodyBlue = document.querySelector('.body-header-blur');
+    const headerLogo = document.querySelector('.header__logo');
+
+    function headerAdd() {
+      headerRight.classList.add('active');
+      bodyBlue.classList.add('active');
+      headerLogo.classList.add('active');
+    }
+
+    function headerRemove() {
+      headerRight.classList.remove('active');
+      bodyBlue.classList.remove('active');
+      headerLogo.classList.remove('active');
+    }
 
     burgerButton.addEventListener('click', function() {
-      headerRight.classList.add('active');
+      headerAdd();
     });
 
     headerClose.addEventListener('click', function() {
-      headerRight.classList.remove('active');
+      headerRemove();
+    });
+
+
+    document.addEventListener('click', function(e) {
+      const target = e.target;
+      const its_menu = target == headerRight || headerRight.contains(target);
+      const its_btnMenu = target == burgerButton;
+        
+      if (!its_menu && !its_btnMenu) {
+        headerRemove();
+      }
     });
   }
 }
