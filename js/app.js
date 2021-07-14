01feae7c1ef56d4187f918f44d9b3aa0c3d8dd50
Vladislav Folkuyan <leonzendero@gmail.com>
2021-07-15 00:28:35 +0300

    'com'

--- js/app.js
+++ js/app.js
@@ -131,4 +131,13 @@ window.onload = function() {
 
   // card tooltip ===
   // =================
+
+  const swiper = new Swiper(".article-slider", {
+    slidesPerView: 3,
+    spaceBetween: 32,
+    navigation: {
+      nextEl: ".article-slider-right",
+      prevEl: ".article-slider-left",
+    },
+  });
 }
